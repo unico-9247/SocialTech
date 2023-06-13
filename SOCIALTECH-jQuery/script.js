9c995369214b2d7d77a0ad8f87a64a7d6660fad2
unico-9247 <monroe0625@gmail.com>
2023-06-14 06:43:33 +0900

    お問い合わせフォームの入力チェックを追加（テキスト#2）

--- SOCIALTECH-jQuery/script.js
+++ SOCIALTECH-jQuery/script.js
@@ -22,4 +22,37 @@ $(function() {
     arrows: false
   });
 
+  // ajaxでSTATIC FORMSにデータを送信
+  $('#sabmit').on('click', function(event) {
+    // formタグによる送信を拒否
+    event.preventDefault();
+
+    // 入力チェックをした結果、エラーがあるかないか判定
+    let result = inputCheck();
+  });
+
+  // フォーカスが外れたとき（blue）にフォームの入力チェックをする
+  $('#name').blur(function() {
+    inputCheck();
+  });
+  $('#furigana').blur(function() {
+    inputCheck();
+  });
+  $('#email').blur(function() {
+    inputCheck();
+  });
+  $('#tel').blur(function() {
+    inputCheck();
+  });
+  $('#message').blur(function() {
+    inputCheck();
+  });
+  $('#agree').blur(function() {
+    inputCheck();
+  });
+
+  // お問い合わせフォームの入力チェック
+  function inputCheck() {
+    console.log('input関数の呼び出し');
+  }
 });
